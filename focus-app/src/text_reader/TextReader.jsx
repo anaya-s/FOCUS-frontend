b36e33bea6089877a6fb98d85b36ba775867320c
brandonaccessmemory <116753343+brandonaccessmemory@users.noreply.github.com>
2025-02-13 15:33:31 +0000

    Websocket connection fix

--- focus-app/src/text_reader/TextReader.jsx
+++ focus-app/src/text_reader/TextReader.jsx
@@ -569,7 +569,7 @@ function TextReaderPage() {
     const token = localStorage.getItem("authTokens"); // Assuming token is stored in localStorage
 
     const cleanBaseURL = baseURL.replace(/^https?:\/\//, ""); // remove 'http://' or 'https://' from baseURL when connecting using WebSocket
-    socket.current = new WebSocket(`ws://${cleanBaseURL}/ws/video/?token=${token}`);
+    socket.current = new WebSocket(`wss://${cleanBaseURL}/ws/video/?token=${token}`);
 
     // console.log("Connecting to WebSocket...");
 
