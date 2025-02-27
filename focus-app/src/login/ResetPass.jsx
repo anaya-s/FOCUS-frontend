33a403bee90a6ae8ada025bfaead98e435abf8a1
brandonaccessmemory <116753343+brandonaccessmemory@users.noreply.github.com>
2025-02-27 14:31:16 +0000

    Email verification

--- focus-app/src/login/ResetPass.jsx
+++ focus-app/src/login/ResetPass.jsx
@@ -13,6 +13,7 @@ function ResetPassword() {
   const [success, setSuccess] = useState(null);
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
+  const uidb64 = searchParams.get("uidb64");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,10 +23,10 @@ function ResetPassword() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/user/reset-password/", {
+      const response = await fetch("http://127.0.0.1:8000/api/user/password-reset-confirm/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ token, password }),
+        body: JSON.stringify({ uidb64, token, password }),
       });
 
       const data = await response.json();
@@ -103,6 +104,7 @@ function ResetPassword() {
                 backgroundColor: "green",
                 "&:hover": { backgroundColor: "darkgreen" },
               }}
+              disabled={!password||!confirmPassword||password!==confirmPassword}
             >
               Reset Password
             </Button>
--- focus-app/src/login/VerifyEmail.jsx
+++ focus-app/src/login/VerifyEmail.jsx
@@ -1,8 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import Box from "@mui/material/Box";
+
+const pageStyle = {
+  display: "flex",
+  height: "100vh",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+};
 
 const VerifyEmail = () => {
   const [searchParams] = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const [status, setStatus] = useState(null);
+
   const token = searchParams.get("token");
   const navigate = useNavigate();
 
@@ -16,16 +28,31 @@ const VerifyEmail = () => {
         .then((response) => response.json())
         .then((data) => {
           if (data.success) {
-            navigate("/login"); // Redirect to login after verification
+            setLoading(false);
+            setStatus(1);
           } else {
-            console.error("Verification failed:", data.error);
+            setLoading(false);
+            setStatus(0);
           }
         })
         .catch((error) => console.error("Error:", error));
     }
   }, [token, navigate]);
 
-  return <div>Verifying your email...</div>;
+
+  return (
+    <Box style={pageStyle}>
+      {loading && <p>Verifying your email...</p>}
+
+      {status === 1 && (
+        <p>Email verified successfully! Start using FOCUS</p>
+      )}
+
+      {status === 0 && (
+        <p>Verification failed. Your token has expired.</p>
+      )}
+    </Box>
+  );
 };
 
 export default VerifyEmail;
